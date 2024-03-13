faa55412eac73ffdf305b2cb283ff05c0786eaad
Egor <Egor@ILWU-MacBook-Air.local>
2024-03-13 17:03:22 +0200

    Prepare for deployment

--- server/server.js
+++ server/server.js
@@ -4,16 +4,12 @@ require("dotenv").config();
 const path = require('path');
 const express = require("express");
 const cors = require("cors");
-const helmet = require("helmet");
-const morgan = require("morgan");
 const applicationRoutes = require("./api/routes/applicationRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(helmet());
-app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/api/applications", applicationRoutes);
